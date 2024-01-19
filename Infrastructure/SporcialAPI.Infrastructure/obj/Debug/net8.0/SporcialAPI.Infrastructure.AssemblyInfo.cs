f3c60b75bf6933da3c44086fa7470175dc527718
onurbbr <onurbuber@engineer.com>
2024-01-19 16:33:50 +0300

    empty

--- Infrastructure/SporcialAPI.Infrastructure/obj/Debug/net8.0/SporcialAPI.Infrastructure.AssemblyInfo.cs
+++ Infrastructure/SporcialAPI.Infrastructure/obj/Debug/net8.0/SporcialAPI.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SporcialAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+364b38bd0a9ceaa225d912acf5d8adb035bfd555")]
 [assembly: System.Reflection.AssemblyProductAttribute("SporcialAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SporcialAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
